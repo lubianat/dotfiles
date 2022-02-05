8b70219ad73e6d7ed80555f71886718bcb47131a
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2022-02-05 07:09:07 -0300

    Update packages to be installed

---
+++ install_ubuntu_packages.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash 
+
+# Install Slack
+wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.0.2-amd64.deb
+sudo apt install ./slack-desktop-*.deb
+
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
