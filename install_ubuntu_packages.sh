6f8731a9b95c760a204ef3174e9a678df1ba4b02
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2022-02-08 15:39:33 -0300

    Update

--- install_ubuntu_packages.sh
+++ install_ubuntu_packages.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash 
 
 sudo 
+
 # Install Slack
-wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.0.2-amd64.deb --no-clobber
-sudo dpkg -i ./slack-desktop-*.deb
+sudo snap install slack --classic
+
+# Install telegram 
+sudo apt install telegram-desktop -y
 
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -16,5 +19,7 @@ sudo apt-get install --reinstall alsa-base pulseaudio -y
 sudo alsa force-reload 
 
 # Remove annoying beep sounds
+sudo sed -i 's/# set bell-style none/set bell-style none/g' /etc/inputrc
 
- sudo sed -i 's/# set bell-style none/set bell-style none/g' /etc/inputrc
+# Install node and yarn
+sudo apt install nodejs yarn -y
