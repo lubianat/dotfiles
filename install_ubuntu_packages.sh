000ee47986bbe16c56c954a34688e8be0cf3b301
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2022-04-05 17:39:21 -0300

    Update install_ubuntu_packages.sh

--- install_ubuntu_packages.sh
+++ install_ubuntu_packages.sh
@@ -117,3 +117,98 @@ sudo dpkg -i "$TEMP_DEB"
 rm -f "$TEMP_DEB"
 
 
+
+#!/bin/bash
+
+basedir="/home/lubianat/Documents"
+
+sudo apt install python3-pip -y
+pip3 install virtualenv
+sudo apt install python3-virtualenv -y
+
+python3 -m virtualenv -p python3.10.4 main_venv
+source $basedir/main_venv/bin/activate 
+pip3 install wbib
+exit 
+# Install apt programs
+AptPackage="curl git zsh 
+telegram-desktop python3-venv 
+nodejs yarn inkscape gparted 
+htop libreoffice libreoffice-style-breeze"
+
+for i in $AptPackage; do
+  sudo apt install $i -y
+done
+
+
+# BB Security Module, GitHub CLI, VScode, Google Chrome
+Urls = 'https://cloud.gastecnologia.com.br/bb/downloads/ws/warsaw_setup64.deb
+https://github.com/cli/cli/releases/download/v2.7.0/gh_2.7.0_linux_amd64.deb
+https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64
+https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+'
+
+for url in $Urls; do
+    TEMP_DEB="$(mktemp)" &&
+wget -O "$TEMP_DEB" $url  &&
+sudo dpkg -i "$TEMP_DEB"
+rm -f "$TEMP_DEB"
+done
+
+read -p 'Login on Github? (y/n): ' uservar
+
+if [ $uservar = "y" ]; then
+    gh auth login
+fi
+
+# Install calibre
+sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Remove annoying beep sounds
+sudo sed -i 's/# set bell-style none/set bell-style none/g' /etc/inputrc
+
+
+# Set up basic directory structure
+mkdir wiki_related lab_related random
+
+# Import core repositories
+
+LabRepos=" gene-wordle bioinfo_brasil"
+cd $basedir/lab_related 
+for val in $LabRepos; do
+    gh repo clone lubianat/$val
+    echo "======================="
+done
+
+
+WikiRepos="wikidata_bib wikidata_cell_curation 
+numista2wikidata coin_herbarium 
+wiki_convida pynaturalist2commons
+bioinfo_brasil"
+cd $basedir/wiki_related 
+for val in $WikiRepos; do
+    gh repo clone lubianat/$val
+    echo "======================="
+done
+
+RandomRepos="dotfiles planning camelo"
+cd $basedir/random 
+for val in $RandomRepos; do
+    gh repo clone lubianat/$val
+    echo "======================="
+done
+
+cd $basedir
+mv $basedir/random/dotfiles/.zshrc ~/.zshrc
+
+
+# Install Python 3.10. Note: do NOT set is as base python
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.10 -y
+sudo apt-get install python3.10-distutils -y
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 2
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
+sudo update-alternatives  --set python3 /usr/bin/python3.8
+curl -sS https://bootstrap.pypa.io/get-pip.py | python3.10
