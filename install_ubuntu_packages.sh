1b4e17a6d63e221a96d0a5fb92413ef8ba77fc02
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2022-04-05 18:44:14 -0300

    Update package installation

--- install_ubuntu_packages.sh
+++ install_ubuntu_packages.sh
@@ -1,135 +1,10 @@
-#!/usr/bin/env bash 
-
-sudo 
-
-# Install curl 
-sudo apt install curl -y
-
-# Install git
-sudo apt install git -y
-
-# Install zsh
-sudo apt install zsh -y
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Install Slack
-sudo snap install slack --classic
-
-# Install telegram 
-sudo apt install telegram-desktop -y
-
-# Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Install venv
-apt install python3-venv -y
-
-# Reinstall audio
-sudo apt-get install --reinstall alsa-base pulseaudio -y
-sudo alsa force-reload 
-
-# Remove annoying beep sounds
-sudo sed -i 's/# set bell-style none/set bell-style none/g' /etc/inputrc
-
-# Install node and yarn
-sudo apt install nodejs yarn -y
-
-# Install inkscape
-sudo apt install inkscape -y
-
-# Install gparted (to manage flashdrives)
-sudo apt install gparted -y
-
-# Make directories
-mkdir lab_related wiki_related random 
-
-# Import core repositories
-
-cd wiki_related 
-gh repo clone lubianat/wikidata_bib
-gh repo clone lubianat/wikidata_cell_curation
-gh repo clone lubianat/numista2wikidata
-gh repo clone lubianat/coin_herbarium
-gh repo clone lubianat/
-gh repo clone lubianat/wiki_convida
-
-
-# Install htop
-sudo apt install htop -y
-
-# Install libreoffice
-sudo apt-get install libreoffice libreoffice-style-breeze -y
-
-# Install calibre
-sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
-
-# Install BB security module
-TEMP_DEB="$(mktemp)" &&
-wget -O "$TEMP_DEB" 'https://cloud.gastecnologia.com.br/bb/downloads/ws/warsaw_setup64.deb' &&
-sudo dpkg -i "$TEMP_DEB"
-rm -f "$TEMP_DEB"
-
-# Install Protege 
-# See https://protegewiki.stanford.edu/wiki/Install_Protege5_Linux
-
-# Configure mouse wheel copy
-sudo apt-get install gnome-tweaks
-echo "xterm*selectToClipboard: true" >> ~/.Xresources
-
-# Install GitHub CLI
-TEMP_DEB="$(mktemp)" &&
-wget -O "$TEMP_DEB" 'https://github.com/cli/cli/releases/download/v2.7.0/gh_2.7.0_linux_amd64.deb' &&
-sudo dpkg -i "$TEMP_DEB"
-rm -f "$TEMP_DEB"
-
-gh auth login
-gh repo clone lubianat/dotfiles
-mv dotfiles/.zshrc ~/.zshrc
-
-# Install Python 3.10
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt update
-sudo apt install python3.10 -y
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 2
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
-
-# Set up main virtualenv
-sudo apt install python3-pip -y
-pip3 install virtualenv
-sudo apt install python3-virtualenv -y
-
-virtualenv -p python3.10.4 main_venv
-source main_venv/bin/activate 
-
-# Install main virtualenv requirements
-pip3 install -r dotfiles/main_venv_requirements.txt
-
-# Install vscode
-TEMP_DEB="$(mktemp)" &&
-wget -O "$TEMP_DEB" "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" &&
-sudo dpkg -i "$TEMP_DEB"
-rm -f "$TEMP_DEB"
-
-# Install Google Chrome
-TEMP_DEB="$(mktemp)" &&
-wget -O "$TEMP_DEB"  "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb" &&
-sudo dpkg -i "$TEMP_DEB"
-rm -f "$TEMP_DEB"
-
-
-
 #!/bin/bash
 
 basedir="/home/lubianat/Documents"
 
-sudo apt install python3-pip -y
-pip3 install virtualenv
-sudo apt install python3-virtualenv -y
+# BB Security Module, GitHub CLI, GitHub Desktop VScode, Google Chrome
 
-python3 -m virtualenv -p python3.10.4 main_venv
-source $basedir/main_venv/bin/activate 
-pip3 install wbib
-exit 
+
 # Install apt programs
 AptPackage="curl git zsh 
 telegram-desktop python3-venv 
@@ -140,10 +15,13 @@ for i in $AptPackage; do
   sudo apt install $i -y
 done
 
+git config --global user.email "tiago.lubiana.alves@usp.br"
+git config --global user.name "Tiago Lubiana"
 
-# BB Security Module, GitHub CLI, VScode, Google Chrome
-Urls = 'https://cloud.gastecnologia.com.br/bb/downloads/ws/warsaw_setup64.deb
+# BB Security Module, GitHub CLI, GitHub Desktop VScode, Google Chrome
+Urls='https://cloud.gastecnologia.com.br/bb/downloads/ws/warsaw_setup64.deb
 https://github.com/cli/cli/releases/download/v2.7.0/gh_2.7.0_linux_amd64.deb
+https://github.com/shiftkey/desktop/releases/download/release-2.9.12-linux4/GitHubDesktop-linux-2.9.12-linux4.deb 
 https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64
 https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 '
@@ -161,19 +39,12 @@ if [ $uservar = "y" ]; then
     gh auth login
 fi
 
-# Install calibre
-sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
-# Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# Remove annoying beep sounds
-sudo sed -i 's/# set bell-style none/set bell-style none/g' /etc/inputrc
-
+logout
 
 # Set up basic directory structure
 mkdir wiki_related lab_related random
 
 # Import core repositories
-
 LabRepos=" gene-wordle bioinfo_brasil"
 cd $basedir/lab_related 
 for val in $LabRepos; do
@@ -200,7 +71,15 @@ for val in $RandomRepos; do
 done
 
 cd $basedir
-mv $basedir/random/dotfiles/.zshrc ~/.zshrc
+cp $basedir/random/dotfiles/.zshrc ~/.zshrc
+
+
+# Install calibre
+sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Remove annoying beep sounds
+sudo sed -i 's/# set bell-style none/set bell-style none/g' /etc/inputrc
 
 
 # Install Python 3.10. Note: do NOT set is as base python
@@ -211,4 +90,57 @@ sudo apt-get install python3.10-distutils -y
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 2
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
 sudo update-alternatives  --set python3 /usr/bin/python3.8
-curl -sS https://bootstrap.pypa.io/get-pip.py | python3.10
+
+sudo apt install python3-pip -y
+pip3 install virtualenv
+sudo apt install python3-virtualenv -y
+
+# Set up virtual environment
+python3 -m virtualenv -p python3.10.4 main_venv --no-pip
+source $basedir/main_venv/bin/activate 
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py 
+python3.10 get-pip.py      
+rm get-pip.py
+
+pip3 install -r $basedir/random/dotfiles/main_venv_requirements.txt
+ 
+# Reinstall audio
+sudo apt-get install --reinstall alsa-base pulseaudio -y
+sudo alsa force-reload 
+
+# Remove annoying beep sounds
+sudo sed -i 's/# set bell-style none/set bell-style none/g' /etc/inputrc
+
+# Install Camelo keyboard
+cd /usr/share/X11/xkb/symbols
+sudo wget -O camelo https://raw.githubusercontent.com/lubianat/camelo/main/camelo
+sudo dpkg-reconfigure xkb-data
+setxkbmap -option grp:alt_shift_toggle camelo,pt
+
+# Install Protege Ontology Editor
+mkdir $basedir/Apps
+cd $basedir/Apps
+wget https://github.com/protegeproject/protege-distribution/releases/download/v5.5.0/Protege-5.5.0-linux.tar.gz
+tar zxvf Protege-5.5.0-linux.tar.gz
+rm Protege-5.5.0-linux.tar.gz
+
+echo "[Desktop Entry]
+Version=1.0
+Exec=$basedir/Apps/Protege-5.5.0/run.sh
+Name=Protege
+GenericName=Protege
+Comment=Opens Protege Ontology Editor
+Encoding=UTF-8
+Terminal=true
+Type=Application
+Categories=Application" > /home/lubianat/Desktop/protege.desktop
+
+
+echo '=========================='
+echo "To set up protege, right-click on 
+the Protege file on the Desktop and 
+select the “Allow launching” option."
+echo '=========================='
+
+read -p 'Ok?' nothing
+
