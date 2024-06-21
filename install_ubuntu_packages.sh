c51b89ee0eac78bcb653490171a61c9180a41507
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2024-06-21 16:29:04 -0300

    Update install_ubuntu_packages.sh

--- install_ubuntu_packages.sh
+++ install_ubuntu_packages.sh
@@ -18,20 +18,6 @@ git config --global user.email "tiago.lubiana.alves@usp.br"
 git config --global user.name "Tiago Lubiana"
 git config --global init.defaultBranch main 
 
-# BB Security Module, GitHub CLI, GitHub Desktop VScode, Google Chrome
-Urls='
-https://zoom.us/client/latest/zoom_amd64.deb
-https://cloud.gastecnologia.com.br/bb/downloads/ws/warsaw_setup64.deb
-https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64
-https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-'
-
-for url in $Urls; do
-    TEMP_DEB="$(mktemp)" &&
-wget -O "$TEMP_DEB" $url  &&
-sudo dpkg -i "$TEMP_DEB"
-rm -f "$TEMP_DEB"
-done
 
 read -p 'Login on Github? (y/n): ' uservar
 
@@ -116,3 +102,16 @@ sudo snap install slack
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# BB Security Module, GitHub CLI, GitHub Desktop VScode, Google Chrome
+Urls='
+https://zoom.us/client/latest/zoom_amd64.deb
+https://cloud.gastecnologia.com.br/bb/downloads/ws/warsaw_setup64.deb
+https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64
+'
+
+for url in $Urls; do
+    TEMP_DEB="$(mktemp)" &&
+wget -O "$TEMP_DEB" $url  &&
+sudo dpkg -i "$TEMP_DEB"
+rm -f "$TEMP_DEB"
+done
